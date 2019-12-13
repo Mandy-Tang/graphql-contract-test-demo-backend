379e80660c451f68ee858e79aba074e9ffdd3c82
Tang Man <189658646@qq.com>
2019-12-13 17:21:29 +0800

    try get and post

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Param } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Get()
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  @Post()
+  create(): string {
+    return 'this action adds a new cat';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `this action returns a ${id} cat`;
   }
 }
