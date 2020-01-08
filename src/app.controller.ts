2dccdcf1abfc82f292df8bf5482e3401b6cbe018
Tang Man <189658646@qq.com>
2020-01-08 13:26:00 +0800

    use jwt in  profile

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,20 @@
 import { AuthService } from './auth/auth.service';
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
   constructor(private readonly authService: AuthService) {}
+
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user)
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  async profile(@Request() req) {
+    return req.user
   }
 }
