b0c8f00c0f6ea64cc08b04c6d5b7a8f0bab0fc74
Tang Man <189658646@qq.com>
2020-01-08 09:54:36 +0800

    fill user service and export it from user module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+export type User = {
+  id: number;
+  username: string;
+  password: string
+}
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [];
+  constructor () {
+    this.users = [
+      {
+        id: 1,
+        username: 'zhansan',
+        password: 'pwd@zhang',
+      },
+      {
+        id: 2,
+        username: 'lisi',
+        password: 'pwd@li',
+      },
+      {
+        id: 3,
+        username: 'wangwu',
+        password: 'pwd@wang',
+      },
+    ];
+  }
+
+  public async findOne(name: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === name)
+  }
+}
